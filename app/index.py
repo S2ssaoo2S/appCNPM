19e6be41dd89c03674046974971a9949857d8147
thanhNhan <iuvklun24@gmail.com>
2025-10-18 00:03:05 +0700

    done 1

--- app/index.py
+++ app/index.py
@@ -1,5 +1,4 @@
 from itertools import product
-
 from flask import Flask, render_template, request
 import dao
 
@@ -10,14 +9,18 @@ app = Flask(__name__)
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("cate_id")
-    print(cate_id)
-    return render_template("index.html", cate=dao.load_catagories(), products=dao.load_product(q, cate_id))
+    print(dao.load_product(q, cate_id))
+    return render_template("index.html",  products=dao.load_product(q, cate_id))
 
 
 @app.route("/products/<int:id>")
 def product_details(id):
     return render_template("products.html",details=dao.load_product_by_id(id))
-
+@app.context_processor
+def context_processor():
+    return {
+        "cate": dao.load_catagories()
+    }
 
 if __name__ == "__main__":
     with app.app_context():
