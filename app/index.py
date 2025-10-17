662c7927fe29c58f28a40c9008a0a29ab62d8ef6
S2ssaoo2S <ltnhan024@gmail.com>
2025-10-17 16:38:38 +0700

    f

---
+++ app/index.py
@@ -0,0 +1,24 @@
+from itertools import product
+
+from flask import Flask, render_template, request
+import dao
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    q = request.args.get("q")
+    cate_id = request.args.get("cate_id")
+    print(cate_id)
+    return render_template("index.html", cate=dao.load_catagories(), products=dao.load_product(q, cate_id))
+
+
+@app.route("/products/<int:id>")
+def product_details(id):
+    return render_template("products.html",details=dao.load_product_by_id(id))
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
